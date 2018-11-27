9f3c4b5ba6963e4b1331d13dc0e2ce2c81a77db3
Andrei <l2.ls@yandex.ru>
2018-11-27 23:54:27 +0300

    Добавлена отрисовка карточки варианта размещения

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@
 
 var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var OFFER_TYPE_NAME = {palace: 'Дворец', flat: 'Квартира', house: 'Дом', bungalo: 'Бунгало'};
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
@@ -14,12 +15,11 @@ var OFFER_MAX_GUESTS = 8;
 var OFFERS_AMOUNT = 8;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
-var LOCATION_X_MIN = 50;
-var LOCATION_X_MAX = document.querySelector('.map').offsetWidth;
-
 
 var mapElement = document.querySelector('.map');
+var mapElementWidth = document.querySelector('.map').offsetWidth;
 var mapPinsElement = document.querySelector('.map__pins');
+var mapFiltersElement = document.querySelector('.map__filters-container');
 var fragment = document.createDocumentFragment();
 
 var getRandomNumber = function (minNumber, maxNumber) {
@@ -38,12 +38,36 @@ var getRandomElement = function (array, cut) {
   return randomElement;
 };
 
-// Исправить
+// Вопрос - функция на каждой итерации должна давать новый массив случайной длины, но вместо этого обновляет исходный массив переданый в параметре "срезая" его длину, почему? Функция вызывается внутри createOffers для свойства features
+//
+// var getOfferFeatures = function (features) {
+//   var featuresList = features;
+//   featuresList.length = Math.floor(Math.random() * (featuresList.length + 1));
+//
+//   return featuresList;
+// };
+
 var getOfferFeatures = function (features) {
-  var featuresList = features;
-  featuresList.length = Math.floor(Math.random() * (featuresList.length + 1));
+  var featuresList = [];
+  var randomAmount = getRandomNumber(1, features.length);
+  for (var i = 0; i < randomAmount; i++) {
+    featuresList.push(features[i]);
+  }
 
   return featuresList;
+};
+
+var shufflePhotos = function (photos) {
+  var photoList = [];
+  for (var i = photos.length - 1; i >= 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var swap = photos[i];
+    photos[i] = photos[j];
+    photos[j] = swap;
+    photoList.push(photos[i]);
+  }
+
+  return photoList;
 };
 
 var showMap = function () {
@@ -55,26 +79,26 @@ var createOffers = function (amount) {
 
   for (var i = 0; i < amount; i++) {
     offers.push({
-      location: {
-        x: getRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX - LOCATION_X_MIN),
-        y: getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX)
-      },
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: getRandomElement(OFFER_TITLES, true),
-        // address:
+        // Вопрос: Как передать в address данные, полученные из свойства location.x/y?
+        address: '',
         price: getRandomNumber(OFFER_MIN_PRICE, OFFER_MAX_PRICE),
         type: getRandomElement(OFFER_TYPE),
         rooms: getRandomNumber(OFFER_MIN_ROOMS, OFFER_MAX_ROOMS),
         guests: getRandomNumber(OFFER_MIN_GUESTS, OFFER_MAX_GUESTS),
         checkin: getRandomElement(CHECKIN_CHECKOUT_TIME),
         checkout: getRandomElement(CHECKIN_CHECKOUT_TIME),
-        // Исправить
         features: getOfferFeatures(OFFER_FEATURES),
         description: '',
-        photos: OFFER_PHOTOS
+        photos: shufflePhotos(OFFER_PHOTOS)
+      },
+      location: {
+        x: getRandomNumber(mapElementWidth * 0.1, mapElementWidth * 0.9),
+        y: getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX)
       }
     });
   }
@@ -82,28 +106,64 @@ var createOffers = function (amount) {
   return offers;
 };
 
-var renderMapPin = function (offer) {
+var createMapPin = function (offerItem) {
   var mapPinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
   var mapPinElement = mapPinTemplate.cloneNode(true);
-
-  mapPinElement.style = 'left: ' + (offer.location.x - mapPinElement.style.width / 2) + 'px; ' + 'top: ' + (offer.location.y - mapPinElement.style.height) + 'px;';
-  mapPinElement.querySelector('img').src = offer.author.avatar;
-  mapPinElement.querySelector('img').alt = offer.offer.title;
+  // Вопрос - Правильно ли получены размеры элементов?? В п.3 задания нужно учесть их размеры
+  mapPinElement.style = 'left: ' + (offerItem.location.x - mapPinElement.style.width / 2) + 'px; ' + 'top: ' + (offerItem.location.y - mapPinElement.style.height) + 'px;';
+  mapPinElement.querySelector('img').src = offerItem.author.avatar;
+  mapPinElement.querySelector('img').alt = offerItem.offer.title;
 
   return mapPinElement;
 };
 
-var renderOffers = function () {
-  var offersList = createOffers(OFFERS_AMOUNT);
-
-  for (var i = 0; i < offersList.length; i++) {
-    fragment.appendChild(renderMapPin(offersList[i]));
+var renderMapPins = function (offers) {
+  for (var i = 0; i < offers.length; i++) {
+    fragment.appendChild(createMapPin(offers[i]));
   }
   mapPinsElement.appendChild(fragment);
+};
 
+var renderOfferCard = function (offerItem) {
+  var offerCardTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.map__card');
+  var offerCard = offerCardTemplate.cloneNode(true);
+
+  offerCard.querySelector('.popup__title').textContent = offerItem.offer.title;
+  offerCard.querySelector('.popup__text--address').textContent = offerItem.offer.address;
+  offerCard.querySelector('.popup__text--price').textContent = offerItem.offer.price + '₽/ночь';
+  offerCard.querySelector('.popup__type').textContent = OFFER_TYPE_NAME[offerItem.offer.type];
+  offerCard.querySelector('.popup__text--capacity').textContent = 'Комнат - ' + offerItem.offer.rooms + ', вмещает гостей - ' + offerItem.offer.guests;
+  offerCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + offerItem.offer.checkin + ', выезд до ' + offerItem.offer.checkout;
+  offerCard.querySelector('.popup__features').textContent = offerItem.offer.features.join(', ');
+  offerCard.querySelector('.popup__description').textContent = offerItem.offer.description;
+  offerCard.querySelector('.popup__avatar').src = offerItem.author.avatar;
+
+  // Как отрисовать фото по-человечески??? :)
+  var photos = offerCard.querySelector('.popup__photos');
+  var photo = offerCard.querySelector('.popup__photo');
+
+  for (var i = 0; i < offerItem.offer.photos.length - 1; i++) {
+    photos.appendChild(photo.cloneNode(true));
+  }
+
+  var photoList = offerCard.querySelectorAll('.popup__photo');
+
+  for (var j = 0; j < photoList.length; j++) {
+    photoList[j].src = offerItem.offer.photos[j];
+  }
+
+  mapElement.insertBefore(offerCard, mapFiltersElement);
+};
+
+var init = function () {
+  var offers = createOffers(OFFERS_AMOUNT);
+  renderMapPins(offers);
+  renderOfferCard(offers[0]);
   showMap();
 };
 
-renderOffers();
+init();
